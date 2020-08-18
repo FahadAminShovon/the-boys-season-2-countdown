06b6f15c17b88cb6fa76f5a2c6d3e72986e09baf
Fahad Shovon <bm.amin.fahad@gmail.com>
2020-08-19 01:07:17 +0600

    :lipstick: Add timer style

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ let i = 1;
 (function timer(){
     const today = new Date();
     const seconds = parseInt((releaseDate - today)/1000);
-    day.innerHTML = parseInt(seconds / 60 / 60/24);
-    hour.innerHTML = parseInt((seconds/60/60)%24);
-    min.innerHTML = parseInt((seconds/60)%60);
-    sec.innerHTML = seconds%60;
+    let ss = seconds % 60;
+    let mm = parseInt((seconds / 60) % 60);
+    let hh = parseInt((seconds / 60 / 60) % 24);
+    let dd = parseInt(seconds / 60 / 60 / 24);
+    day.innerHTML = dd > 9 ? dd : `0${dd}`;
+    hour.innerHTML = hh > 9 ? hh : `0${hh}`;
+    min.innerHTML = mm > 9 ? mm : `0${mm}`;
+    sec.innerHTML = ss > 9 ? ss : `0${ss}`;
     if(seconds > 0)setTimeout(timer, 1000);
     
 }())
